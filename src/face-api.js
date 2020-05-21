f2f516fbd731e0316abf846585d7a11d6e268102
tevin <tevin@tevins-macbook.local>
2020-05-22 02:34:57 +0800

    ok

--- src/face-api.js
+++ src/face-api.js
@@ -1477,13 +1477,9 @@
           var ctx = getContext2dOrThrow(canvasArg);
           var _a = this.options, boxColor = _a.boxColor, lineWidth = _a.lineWidth;
           var _b = this.box, x = _b.x, y = _b.y, width = _b.width, height = _b.height;
-          ctx.strokeStyle = boxColor;
-          ctx.lineWidth = lineWidth;
-          ctx.strokeRect(x, y, width, height);
-          var label = this.options.label;
-          if (label) {
-              new DrawTextField([label], { x: x - (lineWidth / 2), y: y }, this.options.drawLabelOptions).draw(canvasArg);
-          }
+          ctx.strokeStyle = '#f5d59b';
+          ctx.lineWidth = 4;
+          ctx.strokeRect(x, y-50, 200, 200);
       };
       return DrawBox;
   }());
